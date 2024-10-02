4d2ac559f8a43d56ec7546dae6c20279169bc5a5
Tanmay Kalra <tanmaykalra0910@gmail.com>
2024-10-02 16:06:04 +0530

    Added Notification Bell

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,37 +1,37 @@
 // eslint-disable-next-line no-unused-vars
 import React, {useEffect, useState} from "react";
-import { Link, useLocation, useNavigate} from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faPenSquare, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faPenSquare, faUserCircle, faBell } from "@fortawesome/free-solid-svg-icons";
 import backendURL from "../../common/backendUrl";
 import axios from "axios";
 
 const Navbar = () => {
   const path = useLocation().pathname;
   const navigator = useNavigate();
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   
-  useEffect(()=>{
+  useEffect(() => {
     let token = localStorage.getItem("tastytoken");
-   if(token){
-    token = JSON.parse(token) 
-     axios.get(`${backendURL}/api/token`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-     })
-     .then((res)=>{
-       if(res.data.success){
-        setUser(()=>{return {...res.data.user}})
-       }
-     })
-     .catch((err)=>{
-      console.log(err)
-     })
-   }else{
-    setUser(null)
-   }
-  }, [path])
+    if(token) {
+      token = JSON.parse(token);
+      axios.get(`${backendURL}/api/token`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        if(res.data.success) {
+          setUser(() => { return { ...res.data.user } });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    } else {
+      setUser(null);
+    }
+  }, [path]);
 
   return (
     <nav>
@@ -70,25 +70,33 @@ const Navbar = () => {
             >
               Healthy
             </Link>
-          </nav>
+          </nav>gi
           {user === null ? (
-            <Link to={path == "/" || path == "/signup" ? "/login" : "/signup"}>
+            <div className="flex items-center">
+            <Link to={path === "/" || path === "/signup" ? "/login" : "/signup"}>
               <button className="inline-flex items-center bg-red-700 border-0 py-1 px-3 focus:outline-none hover:bg-red-500 rounded text-white mt-4 md:mt-0 transition-all">
-                {path === "/" || path == "/signup" ? "Login" : "Sign Up"}
+                {path === "/" || path === "/signup" ? "Login" : "Sign Up"}
                 <FontAwesomeIcon icon={faArrowRight} className="mx-2" />
               </button>
             </Link>
+            <div onClick={() => navigator(`/notifications`)} className="ml-4 cursor-pointer">
+                <FontAwesomeIcon icon={faBell} className="text-black-700 text-lg" />
+              </div>
+              </div>      
           ) : (
             <div className="flex justify-center items-center">
-              <div onClick={(()=>navigator(`/user/${user._id}`, {state: {user}}))} className="mx-2 cursor-pointer">
+              <div onClick={() => navigator(`/user/${user._id}`, { state: { user } })} className="mx-2 cursor-pointer">
                 <FontAwesomeIcon icon={faUserCircle} className="mx-2 text-red-700 text-lg" />
                 <span className="text-black font-semibold">Profile</span>
               </div>
-              <div onClick={(()=>navigator(`/user/${user._id}/new/recipe`, {state: {user}}))} className="mx-2 cursor-pointer">
+              <div onClick={() => navigator(`/user/${user._id}/new/recipe`, { state: { user } })} className="mx-2 cursor-pointer">
                 <FontAwesomeIcon icon={faPenSquare} className="mx-2 text-red-700 text-lg" />
                 <span className="text-black font-semibold">Add Recipe</span>
               </div>
-            </div>
+              <div onClick={() => navigator(`/notifications`)} className="mx-2 cursor-pointer">
+                <FontAwesomeIcon icon={faBell} className="mx-2 text-black-700 text-lg" />
+              </div>
+            </div>  
           )}
         </div>
       </header>
