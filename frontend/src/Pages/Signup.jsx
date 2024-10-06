fda2fd175439852437fb167a80f602e68281e720
paramveer665 <paramveerchauhan665@gmail.com>
2024-10-06 19:29:56 +0530

    Fixes #9 All Signup Inputs stacked on top of each other.

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -117,108 +117,100 @@ const Signup = () => {
                 Create an Account
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                <div className="flex flex-wrap -m-2">
-                  <div className="px-2 w-full sm:w-1/2">
-                    <div className="relative">
-                      <label
-                        htmlFor="firstName"
-                        className="block text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        First Name
-                      </label>
-                      <input
-                        type="text"
-                        id="firstName"
-                        name="firstName"
-                        className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        placeholder="Ex. Jhon"
-                        value={form.firstName}
-                        onChange={handleChange}
-                        required={true}
-                      />
-                    </div>
-                    {error.firstName && (
-                      <p className="text-red-500 text-sm">
-                        {error.firstNameError}
-                      </p>
-                    )}
-                  </div>
-                  <div className="px-2 sm:w-1/2 w-full">
-                    <div className="relative">
-                      <label
-                        htmlFor="lastName"
-                        className="block text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Last Name
-                      </label>
-                      <input
-                        type="text"
-                        id="lastName"
-                        name="lastName"
-                        className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        placeholder="Ex. Doe"
-                        value={form.lastName}
-                        onChange={handleChange}
-                        required={true}
-                      />
-                    </div>
-                    {error.lastName && (
-                      <p className="text-red-500 text-sm">
-                        {error.lastNameError}
-                      </p>
-                    )}
-                  </div>
+                <div>
+                  <label
+                    htmlFor="firstName"
+                    className="block text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    className="w-full bg-gray-600 rounded border border-gray-300 focus:border-indigo-500 focus:bg-gray-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    placeholder="Ex. John"
+                    value={form.firstName}
+                    onChange={handleChange}
+                    required={true}
+                  />
+                  {error.firstName && (
+                    <p className="text-red-500 text-sm">
+                      {error.firstNameError}
+                    </p>
+                  )}
                 </div>
-                <div className="flex flex-wrap -m-2">
-                  <div className="px-2 sm:w-1/2 w-full">
-                    <div className="relative">
-                      <label
-                        htmlFor="username"
-                        className="block text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Username
-                      </label>
-                      <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        placeholder="Ex. jhonedoe12"
-                        value={form.username}
-                        onChange={handleChange}
-                        required={true}
-                      />
-                    </div>
-                    {error.username && (
-                      <p className="text-red-500 text-sm">
-                        {error.usernameError}
-                      </p>
-                    )}
-                  </div>
-                  <div className="px-2 sm:w-1/2 w-full">
-                    <div className="relative">
-                      <label
-                        htmlFor="email"
-                        className="block text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Email
-                      </label>
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                        placeholder="abc@gmail.com"
-                        value={form.email}
-                        onChange={handleChange}
-                        required={true}
-                      />
-                    </div>
-                    {error.email && (
-                      <p className="text-red-500 text-sm">{error.emailError}</p>
-                    )}
-                  </div>
+
+                <div>
+                  <label
+                    htmlFor="lastName"
+                    className="block text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    className="w-full bg-gray-600 rounded border border-gray-300 focus:border-indigo-500 focus:bg-gray-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    placeholder="Ex. Doe"
+                    value={form.lastName}
+                    onChange={handleChange}
+                    required={true}
+                  />
+                  {error.lastName && (
+                    <p className="text-red-500 text-sm">
+                      {error.lastNameError}
+                    </p>
+                  )}
                 </div>
+
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    className="w-full bg-gray-600 rounded border border-gray-300 focus:border-indigo-500 focus:bg-gray-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    placeholder="Ex. jhonedoe12"
+                    value={form.username}
+                    onChange={handleChange}
+                    required={true}
+                  />
+                  {error.username && (
+                    <p className="text-red-500 text-sm">
+                      {error.usernameError}
+                    </p>
+                  )}
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-gray-600 rounded border border-gray-300 focus:border-indigo-500 focus:bg-gray-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    placeholder="abc@gmail.com"
+                    value={form.email}
+                    onChange={handleChange}
+                    required={true}
+                  />
+                  {error.email && (
+                    <p className="text-red-500 text-sm">{error.emailError}</p>
+                  )}
+                </div>
+
                 <div>
                   <label
                     htmlFor="password"
@@ -232,7 +224,7 @@ const Signup = () => {
                       name="password"
                       id="password"
                       placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-pt-gray-900 focus:border-pt-gray-900 block w-full p-2.5 dark:bg-gray-700 focus:ring- focus:ring-indigo-200 dark:bordet-gray-900 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 transition-colors duration-200 ease-in-out"
+                      className="bg-gray-600 rounded border border-gray-300 text-gray-100 focus:border-indigo-500 focus:bg-gray-500 focus:ring-2 focus:ring-indigo-200 w-full py-1 px-3 transition-colors duration-200 ease-in-out"
                       required={true}
                       value={form.password}
                       onChange={handleChange}
@@ -245,18 +237,19 @@ const Signup = () => {
                       className="absolute top-0 right-0 m-3 cursor-pointer"
                     />
                   </div>
+                  {error.password && (
+                    <div className="text-red-500 text-sm mt-2">
+                      <ul className="list-disc list-inside">
+                        <li>Minimum 8 characters</li>
+                        <li>At least 1 uppercase letter</li>
+                        <li>At least 1 lowercase letter</li>
+                        <li>At least 1 number</li>
+                        <li>At least 1 symbol</li>
+                      </ul>
+                    </div>
+                  )}
                 </div>
-                {error.password && (
-                  <div className="text-red-500 text-sm mt-2">
-                    <ul className="list-disc list-inside">
-                      <li>Minimum 8 characters</li>
-                      <li>At least 1 uppercase letter</li>
-                      <li>At least 1 lowercase letter</li>
-                      <li>At least 1 number</li>
-                      <li>At least 1 symbol</li>
-                    </ul>
-                  </div>
-                )}
+
                 <div>
                   <label
                     htmlFor="cpassword"
@@ -265,21 +258,21 @@ const Signup = () => {
                     Confirm Password
                   </label>
                   <input
-                    type="cpassword"
+                    type="password"
                     name="cpassword"
                     id="cpassword"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-pt-gray-900 focus:border-pt-gray-900 block w-full p-2.5 dark:bg-gray-700 focus:ring- focus:ring-indigo-200 dark:bordet-gray-900 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-600 rounded border border-gray-300 text-gray-100 focus:border-indigo-500 focus:bg-gray-500 focus:ring-2 focus:ring-indigo-200 w-full py-1 px-3 transition-colors duration-200 ease-in-out"
                     required={true}
                     value={form.cpassword}
                     onChange={handleChange}
                   />
+                  {error.cpassword && (
+                    <p className="error text-red-500 text-sm mt-0 mb-2">
+                      {error.cpasswordError}
+                    </p>
+                  )}
                 </div>
-                {error.cpassword && (
-                  <p className="error text-red-500 text-sm mt-0 mb-2">
-                    {error.cpasswordError}
-                  </p>
-                )}
 
                 <button
                   type="submit"
@@ -287,6 +280,7 @@ const Signup = () => {
                 >
                   Sign Up
                 </button>
+
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already Have an Account?{" "}
                   <Link
