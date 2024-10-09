2e7eb14c50c879b6577e17274bd1df8d5396b85a
Tejas Benibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-10-10 02:12:21 +0530

    Grafana and Prometheus Integration in Backend (#145)
    * FEAT: Configuration of Grafana and Prometheus for server monitoring
    
    * Minor Fix
    
    * Update README.md
    
    * minor fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import router from "./routes/web.js";
 import mongoose from "mongoose";
 import { rateLimit } from "./middleware/rateLimit.js";
-
+import client from "prom-client"
 dotenv.config();
 
 const app = express();
@@ -38,10 +38,24 @@ app.use(
     })
 );
 
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics({ register: client.register });
+
 app.use("/api", router);
 
-app.get("/", (req, res) => {
-    res.send("TastyTrails Backend");
+app.get("/", (_, res) => {
+  res.send("TastyTrails Backend");
+});
+
+app.get("/ping", async (_, res) => {
+  res.status(200).json({ message: "pong" });
+});
+
+app.get("/metrics", async (_, res) => {
+  res.setHeader("Content-Type", client.register.contentType);
+  const metrics = await client.register.metrics();
+  res.send(metrics);
 });
 
 // Database Connection and server
