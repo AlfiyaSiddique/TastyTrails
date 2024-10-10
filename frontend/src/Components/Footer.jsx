d4cee517cf32291b1d2af39578b91fa90d942123
Sadath Hussain <126042080+sadath2001@users.noreply.github.com>
2024-10-10 22:45:26 +0530

    Bug footer (#148)
    * first commit
    
    * fixed the footer
    
    * Update server.js
    
    reverted to server.js

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -47,46 +47,47 @@ const Footer = () => {
   };
 
   return (
-    <div>
+  <div className="fixed bottom-0 bg-white  w-full">
       {path !== "/user" && (
         <>
-          <footer className="text-gray-600 body-font">
-            <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-              <span className="flex title-font font-bold items-center md:justify-start justify-center text-red-700">
-                <span className="ml-3 text-xl font-[Mrriweather]">TastyTrails</span>
+        <footer className="text-gray-600 body-font">
+          <div className="container px-2 py-2 flex items-center sm:flex-row flex-col">
+            <span className="flex title-font font-bold items-center md:justify-start justify-center text-red-700">
+              <span className="ml-3 text-xl font-[Mrriweather]">
+                TastyTrails
               </span>
-              <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 sm:text-center">
-                © {new Date().getFullYear()} TastyTrails Developer —
-                <Link
-                  to="https://twitter.com/A_l_f_i_y_a"
-                  className="text-gray-600 ml-1 sm:text-center"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  @A_l_f_i_y_A
-                </Link>
-              </p>
-              <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                <Link
-                  to={"https://www.instagram.com/alfiya.17.siddiq/"}
-                  className="text-ref-500 text-red-700"
-                >
-                  <FontAwesomeIcon icon={faInstagramSquare} />
-                </Link>
-                <Link
-                  to={"https://www.linkedin.com/in/alfiya-siddique-987a59240/"}
-                  className="ml-3 text-red-700"
-                >
-                  <FontAwesomeIcon icon={faLinkedinIn} className="" />
-                </Link>
-                <Link
-                  to={"https://github.com/AlfiyaSiddique"}
-                  className="ml-3 text-red-700"
-                >
-                  <FontAwesomeIcon icon={faGithubSquare} />
-                </Link>
-              </span>
-
+            </span>
+            <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 sm:text-center">
+              © {new Date().getFullYear()} TastyTrails Developer —
+              <Link
+                to="https://twitter.com/A_l_f_i_y_a"
+                className="text-gray-600 ml-1 sm:text-center"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                @A_l_f_i_y_A
+              </Link>
+            </p>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+              <Link
+                to={"https://www.instagram.com/alfiya.17.siddiq/"}
+                className="text-ref-500 text-red-700"
+              >
+                <FontAwesomeIcon icon={faInstagramSquare} />
+              </Link>
+              <Link
+                to={"https://www.linkedin.com/in/alfiya-siddique-987a59240/"}
+                className="ml-3 text-red-700"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} className="" />
+              </Link>
+              <Link
+                to={"https://github.com/AlfiyaSiddique"}
+                className="ml-3 text-red-700"
+              >
+                <FontAwesomeIcon icon={faGithubSquare} />
+              </Link>
+            </span>
               {/* Contact Us / Rate Us Button */}
               <button
                 onClick={() => setShowModal(true)}
@@ -94,9 +95,8 @@ const Footer = () => {
               >
                 Feedback
               </button>
-            </div>
-          </footer>
-
+          </div>
+        </footer>
           {/* Modal */}
           {showModal && (
             <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
