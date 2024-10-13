a8afdef569e7c59c7075fa78a26fee7789caaa4d
Alfiya Siddique <86224794+AlfiyaSiddique@users.noreply.github.com>
2024-10-13 22:17:28 +0530

    Revert "Feature/feedback : #167 done  (#181)"
    This reverts commit 30acf8102d9c30b4d75cebd5be58d79ed36925fe.

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCheckCircle,
-  faTimesCircle,
-} from "@fortawesome/free-solid-svg-icons"; // Import success and error icons
-import {
   faInstagramSquare,
   faLinkedinIn,
   faGithubSquare,
@@ -18,9 +14,8 @@ const Footer = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [submitStatus, setSubmitStatus] = useState(null);
   const [isSticky, setIsSticky] = useState(false);
-  const backendURL = import.meta.env.VITE_BACKEND_URL;
+
   const path = useLocation().pathname;
 
   // Check if the content is smaller than the screen
@@ -42,222 +37,142 @@ const Footer = () => {
     setRating(rate);
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+
     if (!name || !email || !message || rating === 0) {
       alert("Name, Email, Message, and Rating are mandatory fields!");
       return;
     }
+    toast.success("Feedback submitted succesfully.");
 
-    const formData = {
+    console.log("Submitted Data:", {
       name,
       email,
       message,
       rating,
-    };
-
-    try {
-      //This is the URL you can use if you are working on local machine if creating error in finding backendURL so use this in fetch request = 'http://localhost:8080/api/feedback
-      const response = await fetch(`${backendURL}/api/feedback`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      const result = await response.json();
-      if (response.ok) {
-        setSubmitStatus('success');
-      } else {
-        setSubmitStatus('error');
-      }
-    } catch (error) {
-      console.error("Error sending form data:", error);
-      setSubmitStatus('error');
-    }
-  };
-
-  const openModal = () => {
-    setSubmitStatus(null);
-    setShowModal(true);
+    });
 
     setName("");
     setEmail("");
     setMessage("");
     setRating(0);
-    setShowModal(true);
+    setShowModal(false);
   };
 
   return (
-    <div className="fixed bottom-0 bg-white  w-full">
+    <div className={`${isSticky ? "fixed bottom-0 w-full" : ""} bg-white`}>
       {path !== "/user" && (
         <>
           <footer className="text-gray-600 body-font">
-            <div className="container px-2 py-2 flex items-center sm:flex-row flex-col">
-              <span className="flex title-font font-bold items-center md:justify-start justify-center text-red-700">
-                <span className="ml-3 text-xl font-[Mrriweather]">
-                  TastyTrails
-                </span>
+            <div className="container mx-auto px-5 py-6 flex flex-col sm:flex-row justify-between items-center">
+              <span className="flex title-font font-bold items-center text-red-700">
+                <span className="ml-3 text-xl font-[Mrriweather]">TastyTrails</span>
               </span>
-              <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 sm:text-center">
+              <p className="text-sm text-gray-500 mt-4 sm:mt-0">
                 © {new Date().getFullYear()} TastyTrails Developer —
                 <Link
                   to="https://twitter.com/A_l_f_i_y_a"
-                  className="text-gray-600 ml-1 sm:text-center"
+                  className="text-gray-600 ml-1"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
                   @A_l_f_i_y_A
                 </Link>
               </p>
-              <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                <Link
-                  to={"https://www.instagram.com/alfiya.17.siddiq/"}
-                  className="text-ref-500 text-red-700"
-                >
+              <div className="flex justify-center sm:justify-start space-x-4 mt-4 sm:mt-0">
+                <Link to="https://www.instagram.com/alfiya.17.siddiq/" className="text-red-700">
                   <FontAwesomeIcon icon={faInstagramSquare} />
                 </Link>
-                <Link
-                  to={"https://www.linkedin.com/in/alfiya-siddique-987a59240/"}
-                  className="ml-3 text-red-700"
-                >
-                  <FontAwesomeIcon icon={faLinkedinIn} className="" />
+                <Link to="https://www.linkedin.com/in/alfiya-siddique-987a59240/" className="text-red-700">
+                  <FontAwesomeIcon icon={faLinkedinIn} />
                 </Link>
-                <Link
-                  to={"https://github.com/AlfiyaSiddique"}
-                  className="ml-3 text-red-700"
-                >
+                <Link to="https://github.com/AlfiyaSiddique" className="text-red-700">
                   <FontAwesomeIcon icon={faGithubSquare} />
                 </Link>
-              </span>
-              {/* Contact Us / Rate Us Button */}
+              </div>
+              {/* Feedback Button */}
               <button
-                onClick={openModal} // Call openModal when feedback button is clicked
+                onClick={() => setShowModal(true)}
                 className="ml-4 py-2 px-4 bg-transparent border border-red-700 text-red-700 rounded hover:bg-red-700 hover:text-white"
               >
                 Feedback
               </button>
             </div>
           </footer>
-
           {/* Modal */}
           {showModal && (
             <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
               <div className="bg-white p-6 rounded-lg w-full max-w-lg">
-                {submitStatus === null ? (
-                  <>
-                    {/* Show the form if no status has been set */}
-                    <h2 className="text-2xl font-bold mb-4">Feedback</h2>
-                    <form className="space-y-4" onSubmit={handleSubmit}>
-                      <div>
-                        <label className="block text-gray-700">Name</label>
-                        <input
-                          type="text"
-                          className="w-full px-4 py-2 border border-gray-300 rounded"
-                          placeholder="Your Name"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                          required
-                        />
-                      </div>
-
-                      <div>
-                        <label className="block text-gray-700">Email</label>
-                        <input
-                          type="email"
-                          className="w-full px-4 py-2 border border-gray-300 rounded"
-                          placeholder="Your Email"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                          required
-                        />
-                      </div>
-
-                      <div>
-                        <label className="block text-gray-700">Message</label>
-                        <textarea
-                          className="w-full px-4 py-2 border border-gray-300 rounded"
-                          placeholder="Your Message"
-                          value={message}
-                          onChange={(e) => setMessage(e.target.value)}
-                          required
-                        ></textarea>
-                      </div>
-
-                      <div>
-                        <label className="block text-gray-700">Rate Us</label>
-                        <div className="flex text-2xl">
-                          {[1, 2, 3, 4, 5].map((star) => (
-                            <span
-                              key={star}
-                              className={`cursor-pointer ${rating >= star
-                                ? "text-yellow-400"
-                                : "text-gray-400"
-                                }`}
-                              onClick={() => handleRating(star)}
-                            >
-                              {rating >= star ? "★" : "☆"}
-                            </span>
-                          ))}
-                        </div>
-                        {!rating && (
-                          <p className="text-red-600 text-sm">
-                            Rating is required!
-                          </p>
-                        )}
-                      </div>
-
-                      <div className="flex justify-end">
-                        <button
-                          type="button"
-                          className="mr-4 py-2 px-4 bg-red-500 text-white rounded hover:bg-red-700"
-                          onClick={() => setShowModal(false)}
-                        >
-                          Close
-                        </button>
-                        <button
-                          type="submit"
-                          className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700"
-                        >
-                          Submit
-                        </button>
-                      </div>
-                    </form>
-                  </>
-                ) : submitStatus === 'success' ? (
-                  <div className="text-center">
-                    <FontAwesomeIcon
-                      icon={faCheckCircle}
-                      className="text-green-500 text-5xl mb-4"
+                <h2 className="text-2xl font-bold mb-4">Feedback</h2>
+                <form className="space-y-4" onSubmit={handleSubmit}>
+                  <div>
+                    <label className="block text-gray-700">Name</label>
+                    <input
+                      type="text"
+                      className="w-full px-4 py-2 border border-gray-300 rounded"
+                      placeholder="Your Name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
                     />
-                    <h2 className="text-2xl font-bold mb-4">
-                      Feedback sent successfully!
-                    </h2>
+                  </div>
+
+                  <div>
+                    <label className="block text-gray-700">Email</label>
+                    <input
+                      type="email"
+                      className="w-full px-4 py-2 border border-gray-300 rounded"
+                      placeholder="Your Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label className="block text-gray-700">Message</label>
+                    <textarea
+                      className="w-full px-4 py-2 border border-gray-300 rounded"
+                      placeholder="Your Message"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <label className="block text-gray-700">Rate Us</label>
+                    <div className="flex text-2xl">
+                      {[1, 2, 3, 4, 5].map((star) => (
+                        <span
+                          key={star}
+                          className={`cursor-pointer ${rating >= star ? "text-yellow-400" : "text-gray-400"}`}
+                          onClick={() => handleRating(star)}
+                        >
+                          {rating >= star ? "★" : "☆"}
+                        </span>
+                      ))}
+                    </div>
+                    {!rating && <p className="text-red-600 text-sm">Rating is required!</p>}
+                  </div>
+
+                  <div className="flex justify-end">
                     <button
-                      className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700"
+                      type="button"
+                      className="mr-4 py-2 px-4 bg-red-500 text-white rounded hover:bg-red-700"
                       onClick={() => setShowModal(false)}
                     >
                       Close
                     </button>
-                  </div>
-                ) : (
-                  <div className="text-center">
-                    <FontAwesomeIcon
-                      icon={faTimesCircle}
-                      className="text-red-500 text-5xl mb-4"
-                    />
-                    <h2 className="text-2xl font-bold mb-4">
-                      Error in sending Feedback!
-                    </h2>
                     <button
+                      type="submit"
                       className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700"
-                      onClick={() => setShowModal(false)}
                     >
-                      Close
+                      Submit
                     </button>
                   </div>
-                )}
+                </form>
               </div>
             </div>
           )}
