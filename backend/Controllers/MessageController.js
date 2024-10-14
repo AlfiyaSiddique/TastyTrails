0d245869788a623fea1c6a99ccdc828a09300ba6
sadath2001 <126042080+sadath2001@users.noreply.github.com>
2024-10-14 17:24:14 +0530

    backend routes setup

---
+++ backend/Controllers/MessageController.js
@@ -0,0 +1,62 @@
+import asyncHandler from "express-async-handler";
+import Message from "../models/Message.js";
+import Chat from "../models/Chat.js";
+import User from "../models/User.js";
+
+
+//@description     Get all Messages
+//@route           GET /api/Message/:chatId
+//@access          Protected
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name pic email")
+      .populate("chat");
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+//@description     Create New Message
+//@route           POST /api/Message/
+//@access          Protected
+const sendMessage = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
+
+  var newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    var message = await Message.create(newMessage);
+
+    message = await message.populate("sender", "name pic").execPopulate();
+    message = await message.populate("chat").execPopulate();
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const MessageController={
+allMessages, sendMessage
+}
+
+export default ChatController
---
+++ backend/models/Chat.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const chatModel=mongoose.Schema(
+    {
+        chatName:{
+            type:String,
+            trim:true
+        },
+        isGroupChat:{
+            type:Boolean,
+            default:false
+        },
+        users:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }],
+        latestMessage:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Message"        
+        },
+        groupAdmin:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User" 
+        }
+
+    },
+    {
+        timestamps:true,
+    }
+);
+
+const Chat=mongoose.model("Chat",chatModel);
+
+export default Chat;
