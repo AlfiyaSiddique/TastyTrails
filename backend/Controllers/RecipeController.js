4d0abd7063eb1affda0e4860d06ea1ca8e27cef0
Saurbh Kumar <Saurbhbailara@gmail.com>
2024-10-21 14:03:59 +0530

    Update RecipeController.js

--- backend/Controllers/RecipeController.js
+++ backend/Controllers/RecipeController.js
@@ -364,7 +364,6 @@ const getComments = async (req, res) => {
 
     // Fetch all comments for this recipe
     const comments = await Comment.find({ recipe: recipeId }).select('username content date').sort({ date: -1 });
-    
     return res.status(200).json({ success: true, comments });
   } catch (error) {
     console.log(error);
