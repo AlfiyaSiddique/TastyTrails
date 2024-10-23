b0d6d9b22ad87ef31bd027073d6680eff15218b9
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-10-23 19:25:33 +0530

    resolved conflict

--- backend/routes/web.js
+++ backend/routes/web.js
@@ -7,15 +7,9 @@ import authenticateToken from "../middleware/auth.js";
 const router = Router();
 
 // Get Requests
-<<<<<<< HEAD
-router.get('/usernames', UserController.getAllUserName)
-router.get('/token', authenticateToken, UserController.verifyUserByToken)
-router.get("/recipes", RecipeController.allRecipe)
-=======
 router.get("/usernames", UserController.getAllUserName);
 router.get("/token", authenticateToken, UserController.verifyUserByToken);
 router.get("/recipes", RecipeController.allRecipe);
->>>>>>> 2c3a6840f60182553c9345c7f4a9af5057c8f6a0
 // added route to get previous comments
 router.get("/recipe/getcomments/:recipeId", RecipeController.getComments);
 
@@ -32,12 +26,6 @@ router.post(
 router.post("/recipe/delete", authenticateToken, RecipeController.deleteRecipe);
 router.post("/get-recipe", ChatbotController.getRecipeSuggestions);
 // added route to add new comment to database
-<<<<<<< HEAD
-router.post("/recipe/addcomment", authenticateToken, RecipeController.addComment)
-router.post('/feedback', UserController.Sendcontactmail);
-router.patch('/recipe/share/:recipeId', authenticateToken, RecipeController.updateShareCount)
-router.get('/recipe/:recipeId', RecipeController.getRecipeById)
-=======
 router.post(
   "/recipe/addcomment",
   authenticateToken,
@@ -45,5 +33,4 @@ router.post(
 );
 router.post("/feedback", UserController.Sendcontactmail);
 
->>>>>>> 2c3a6840f60182553c9345c7f4a9af5057c8f6a0
 export default router;
