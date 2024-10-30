91c5efaf48b9a261bb4dce2c826d09704f5401f8
sushil-sagar05 <sagarsushil1403@gmail.com>
2024-10-30 23:06:11 +0530

    updating all changes

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -151,7 +151,7 @@ const Signup = () => {
 
 
   const googleSignup = useGoogleAuth(handleGoogleSignup, true);
-
+return(
     <div>
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -355,8 +355,8 @@ const Signup = () => {
   </div>
 
 
-
-
+              );
+              };
 export default Signup
 
 
