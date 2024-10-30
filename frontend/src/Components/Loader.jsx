a5136b869082703c514439bdaa1b35c4a5ffdd1f
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-10-30 21:12:34 +0530

    updated loader page

--- frontend/src/Components/Loader.jsx
+++ frontend/src/Components/Loader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Loader() {
     return (
-        <div className="w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="w-16 h-16 border-4 border-blue-500 border-solid border-t-transparent rounded-full animate-spin"></div>
+        <div className="w-screen h-screen fixed top-0 left-0 bg-white bg-opacity-850 flex items-center justify-center z-50">
+            <div className='text-2xl font-bold text-gray-800 animate-bounce'>Deliciousness loading, please hold...</div>
         </div>
     )
 }
