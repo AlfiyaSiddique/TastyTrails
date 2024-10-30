194a33a951d67b4034f771577ac514947ac7d674
haseebzaki-07 <haseebzaki13@gmail.com>
2024-10-30 14:48:11 +0530

    add seed data

--- frontend/src/Pages/Recipes.jsx
+++ frontend/src/Pages/Recipes.jsx
@@ -33,34 +33,38 @@ const Recipes = ({ type }) => {
 
   useEffect(() => {
     const fetchRecipes = async () => {
-      try {
-        const response = await axios.get(`${backendURL}/api/recipes`, {
-          params: {
-            page: pagination.currentPage,
-            limit,
-            type,
-            search: debouncedSearchTerm,
-          },
-        });
+        
+        try {
+            const response = await axios.get(`${backendURL}/api/recipes`, {
+                params: {
+                    page: pagination.currentPage,
+                    limit,
+                    type,
+                    search: debouncedSearchTerm,
+                },
+            });
 
-        const { recipes, pagination: paginationData } = response.data;
+            console.log("Response data:", response.data); // Add this line
 
-        setRecipes(recipes);
+            const { recipes, pagination: paginationData } = response.data;
 
-        setPagination((prev) => ({
-          ...prev,
-          totalRecipes: paginationData.totalRecipes,
-          totalPages: paginationData.totalPages
-        }));
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
-      }
+            setRecipes(recipes);
+
+            setPagination((prev) => ({
+                ...prev,
+                totalRecipes: paginationData.totalRecipes,
+                totalPages: paginationData.totalPages
+            }));
+        } catch (err) {
+            console.error("Error fetching recipes:", err); 
+        } finally {
+            setLoading(false);
+        }
     };
 
     fetchRecipes();
-  }, [pagination.currentPage, type, debouncedSearchTerm]);
+}, [pagination.currentPage, type, debouncedSearchTerm]);
+
 
   const handleSearch = (e) => {
     const value = e.target.value;
