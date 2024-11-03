966b4808e6d24ac56a85c173bd757a63b8548494
Ikki <vivekprakash.st@gmail.com>
2024-11-04 01:28:22 +0530

    added-feedback-deletion-frontend

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { toast } from "react-toastify";
 import Cards from "../Components/Cards"; // Import Cards component
-
+import { Star } from "lucide-react";
 const Dashboard = () => {
   // Routes hooks and passed data
   const navigator = useNavigate();
@@ -15,12 +15,17 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(true);
   const [recipes, setRecipes] = useState([]);
   const [error, setError] = useState(null); // Track any errors
+  const [feedbacks, setFeedbacks] = useState([]);
   const [likedRecipes, setLikedRecipes] = useState([]);
-  const [viewingLikedRecipes, setViewingLikedRecipes] = useState(false); // Track if we are viewing liked recipes
   const [imagePreview, setImagePreview] = useState(
     "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
   ); // default image to preview
-
+  const ViewState = {
+    RECIPE: 'RECIPE',
+    LIKED_RECIPE: 'LIKED_RECIPE',
+    FEEDBACK: 'FEEDBACK',
+  };
+  const [viewingState, setViewingState] = useState(ViewState.RECIPE);
   const inputFile = useRef(null); // for redirecting click to open input file
   
   // form to send image change request
@@ -82,16 +87,71 @@ const Dashboard = () => {
       });
   };
 
+  const fetchFeedbacks = () => {
+    setLoading(true);
+   
+    fetch(`${backendURL}/api/feedback/${user._id}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch feedbacks.');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFeedbacks(data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching feedbacks:", error);
+        setError("Failed to fetch feedbacks. Please try again.");
+      })
+      .finally(() => setLoading(false)); // Set loading to false after fetching
+  };
+  
+  const handleFeedbackDelete = (id)=>{
+    const val = confirm("Are you sure you want to delete this feedback?");
+    if (val) {
+      axios
+        .post(
+          `${backendURL}/api/feedback/delete`,
+          { id: id },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          if (res.data.success) {
+            toast.success("Feedback Deleted Successfully");
+            setFeedbacks((prevFeedbacks) =>
+              prevFeedbacks.filter((feedback) => feedback._id !==   id)
+            );
+          } else {
+            toast.error("Failed to delete feedback. Please try again later.");
+          }
+        })
+        .catch((err) => {
+          toast.error(`Error deleting the feedback: ${err.message}`);
+        });
+    }
+    
+  }
   useEffect(() => {
     if (user._id) {
       fetchRecipes(); // Only fetch recipes if user ID is available
       fetchUserImage(); // fetch user image if ID available
+      fetchFeedbacks();
     } else {
       console.error("User ID is missing!");
       setError("User data is not available.");
     }
   }, [user._id]);
-
+  
   const handleDelete = (id) => {
     const val = confirm("Are you sure you want to delete this recipe?");
     if (val) {
@@ -176,22 +236,30 @@ const Dashboard = () => {
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-start text-base justify-star border-b border-gray-200">
             <span
               className={`mr-5 hover:text-gray-900 cursor-pointer border-b pb-1 ${
-                !viewingLikedRecipes ? "border-red-700" : ""
+                viewingState === ViewState.RECIPE ? "border-red-700" : ""
               }`}
-              onClick={() => setViewingLikedRecipes(false)}
+              onClick={() => setViewingState(ViewState.RECIPE)}
             >
               My Recipe
             </span>
             <span
               className={`mr-5 hover:text-gray-900 cursor-pointer ${
-                viewingLikedRecipes ? "border-red-700" : ""
+                viewingState === ViewState.LIKED_RECIPE ? "border-red-700" : ""
               }`}
               onClick={() => {
-                setViewingLikedRecipes(true);
+                setViewingState(ViewState.LIKED_RECIPE);
                 fetchLikedRecipes(); // Fetch liked recipes when the tab is clicked
               }}
             >
               Liked Recipe
+            </span>
+            <span
+              className={`mr-5 hover:text-gray-900 cursor-pointer border-b pb-1 ${
+                (viewingState == ViewState.FEEDBACK) ? "border-red-700" : ""
+              }`}
+              onClick={() => setViewingState(ViewState.FEEDBACK)}
+            >
+              Feedbacks
             </span>
           </nav>
           <section className="text-gray-600 body-font overflow-hidden">
@@ -200,12 +268,11 @@ const Dashboard = () => {
                 <div>Loading...</div>
               ) : (
                 <div className="-my-8 divide-y-2 divide-gray-100">
-                  {viewingLikedRecipes
-                    ? likedRecipes.map((recipe) => (
+                  {viewingState === ViewState.LIKED_RECIPE && likedRecipes.map((recipe) => (
                         <Cards key={recipe._id} dish={recipe} />
                         // Ensure recipe._id is unique
-                      ))
-                    : recipes.map((recipe) => (
+                      )) }
+                    {viewingState === ViewState.RECIPE && recipes.map((recipe) => (
                         <div
                           key={recipe._id}
                           className="cursor-pointer py-8 flex flex-wrap md:flex-nowrap"
@@ -248,6 +315,15 @@ const Dashboard = () => {
                           </div>
                         </div>
                       ))}
+                  {viewingState === ViewState.FEEDBACK &&
+                    feedbacks.map((feedback) => (
+                      <div
+                        key={feedback._id}
+                        className="py-4"
+                      >
+                       <FeedbackCard feedback={feedback} handleFeedbackDelete={handleFeedbackDelete}/>
+                      </div>
+                    ))}
                 </div>
               )}
             </div>
@@ -309,4 +385,31 @@ const Dashboard = () => {
   );
 };
 
+const FeedbackCard = ({feedback, handleFeedbackDelete})=>{
+  return(
+  <div className="rounded-lg mr-5 w-2/3 p-4 flex flex-col justify-between h-full transition-shadow duration-300 transform hover:scale-105 shadow-[0_2px_10px_rgba(0,0,0,0.3)] bg-white">
+    <div>
+      <div className="flex items-center mb-4">
+        <img src={feedback.userId.profile} alt={feedback.userId.firstName} className="w-16 h-16 rounded-full border-2 mr-4 object-cover" />
+        <div>
+          <h3 className="font-semibold text-lg text-gray-800">{feedback.userId.firstName} {feedback.userId.lastName}</h3>
+          <p className="text-sm text-red-600">{feedback.role}</p>
+        </div>
+        <div className="cursor-pointer absolute top-4 right-4 cursor-pointer" onClick={()=>handleFeedbackDelete(feedback._id)}>
+
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="35" height="35" viewBox="0 0 72 72" style={{"fill" :"#FA5252"}}>
+<path d="M 32.5 9 C 28.364 9 25 12.364 25 16.5 L 25 18 L 17 18 C 14.791 18 13 19.791 13 22 C 13 24.209 14.791 26 17 26 L 17.232422 26 L 18.671875 51.916016 C 18.923875 56.449016 22.67875 60 27.21875 60 L 44.78125 60 C 49.32125 60 53.076125 56.449016 53.328125 51.916016 L 54.767578 26 L 55 26 C 57.209 26 59 24.209 59 22 C 59 19.791 57.209 18 55 18 L 47 18 L 47 16.5 C 47 12.364 43.636 9 39.5 9 L 32.5 9 z M 32.5 16 L 39.5 16 C 39.775 16 40 16.224 40 16.5 L 40 18 L 32 18 L 32 16.5 C 32 16.224 32.225 16 32.5 16 z M 36 28 C 37.104 28 38 28.896 38 30 L 38 47.923828 C 38 49.028828 37.104 49.923828 36 49.923828 C 34.896 49.923828 34 49.027828 34 47.923828 L 34 30 C 34 28.896 34.896 28 36 28 z M 27.392578 28.001953 C 28.459578 27.979953 29.421937 28.827641 29.460938 29.931641 L 30.085938 47.931641 C 30.123938 49.035641 29.258297 49.959047 28.154297 49.998047 C 28.131297 49.999047 28.108937 50 28.085938 50 C 27.012938 50 26.125891 49.148359 26.087891 48.068359 L 25.462891 30.068359 C 25.424891 28.964359 26.288578 28.040953 27.392578 28.001953 z M 44.607422 28.001953 C 45.711422 28.039953 46.575109 28.964359 46.537109 30.068359 L 45.912109 48.068359 C 45.874109 49.148359 44.986063 50 43.914062 50 C 43.891062 50 43.868703 49.999047 43.845703 49.998047 C 42.741703 49.960047 41.876063 49.035641 41.914062 47.931641 L 42.539062 29.931641 C 42.577062 28.827641 43.518422 27.979953 44.607422 28.001953 z"></path>
+</svg>
+        </div>
+      </div>
+      <p className="text-xl font-bold mb-2 text-gray-800">"{feedback.quote}"</p>
+      <p className="mb-4 text-gray-600">{feedback.review}</p>
+    </div>
+    <div className="flex items-center">
+      {[...Array(5)].map((_, i) => (
+        <Star key={i} className={`w-5 h-5 ${i < feedback.rating ? 'text-yellow-400 fill-current' : 'text-gray-300'}`} />
+      ))}
+    </div>
+  </div>
+)}
 export default Dashboard;
