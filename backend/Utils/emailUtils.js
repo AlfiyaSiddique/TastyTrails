0c5f1ddccfd67a76fa0b6e351a0e4cfed0ea4ccf
Alfiya Siddique <86224794+AlfiyaSiddique@users.noreply.github.com>
2024-11-09 19:04:53 +0530

    Update emailUtils.js

--- backend/Utils/emailUtils.js
+++ backend/Utils/emailUtils.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 export const sendVerificationEmail = async (email, token, jwt, user_id) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail', // Use your email provider here
+    service: process.env.EMAIL_PROVIDER, // Use your email provider here
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
